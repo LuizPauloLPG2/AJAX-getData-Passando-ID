0fc2d664c3e5c10dd504ad93d39431711cc6a33b
Luiz Paulo Gonçalves <37484860+LuizPauloLPG2@users.noreply.github.com>
2018-10-04 10:43:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".setIdCliente").on("click", function () {
+    $(".setIdFuncionario").on("click", function () {
         var id = this.dataset.id;
         $.ajax({
             url: 'ajax.php',
@@ -14,50 +14,3 @@ $(document).ready(function () {
         });
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//$(document).ready(function () {
-//    $("#sendProdutos").on("click", function () {
-//        var id = this.dataset.id;
-//        $.ajax({
-//            url: 'produtos.php',
-//            data: {
-//                id: id
-//            }
-//        }).done(function (data) {
-//            produto = JSON.parse(data);
-//            var html = "";
-//            for (var i = 0; i < produto.length; i++) {
-//                produto[i];
-//
-//                var nome = produto[i].nome_produto;
-//                console.log(produto[i].nome_produto)
-//                html += "NOME: " + nome + " <br>";
-//                html += "--------------------<br>";
-//            }
-//            var popula = document.querySelector("#populaProduto");
-//            popula.innerHTML = html;
-//        });
-//    });
-//});
